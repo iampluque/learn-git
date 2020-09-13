516019a59265e7a09851329d3e421831172b9af0
pluqueTheLuxe <pierre-luc.simard@videotron.ca>
2020-09-13 14:04:38 -0400

    is this a conflict

--- demo.sh
+++ demo.sh
@@ -1,4 +1,6 @@
-init-session() {
+sessionsDir=./sessions
+
+init_session() {
     git init &> /dev/null
     #wireing: do not take note
     git config alias.adog "log --all --decorate --oneline --graph"  &> /dev/null
@@ -9,26 +11,33 @@ EOF
     git add . &> /dev/null
     git commit -am "initial commit" &> /dev/null
 }
+repo_state() {
+    echo
+    echo Current repo state is ...
+    git adog
+}
 proceed() {
     echo
     read -p "Press any key to proceed the next step..."
     echo
 }
-
-runfile=./sessions/$1.yml
-
-if ! test -f "$runfile"; then
-    echo no test case
-    return 1
-fi
-
-rm -rf demo
-mkdir demo
-
-cp $runfile ./demo/config.yml
-
-cat <<"EOF" > ./demo/run.sh
+list_sessions() {
+    for file in $(cd $sessionsDir && ls *.yml)
+    do
+        echo "  - ${file%%.*}"
+    done
+}
+# get_session() {
+#     old="$IFS"
+#     IFS='-'
+#     echo "$*"
+#     IFS=$old
+# }
+copy_runfile() {
+    cat <<"EOF" > $1
 echo "$(yq r config.yml intro)"
+init_session
+repo_state
 
 stepsCount=$(yq r config.yml --length steps)
 for (( step = 0; step <= $stepsCount - 1; step++ ))
@@ -36,15 +45,33 @@ do
     proceed
 
     eval "$(yq r config.yml steps[$step].commands)"
+    eval "$(yq r config.yml steps[$step].outputs)"
 
     echo "$(yq r config.yml steps[$step].title) done!"
-    echo Current repo state is ...
-    git adog
+    repo_state
 done
 
 echo
 echo Session done!
 echo
 EOF
+}
+
+session=$1 #$(get_session $@)
+configFile=$sessionsDir/$session.yml
+
+if ! test -f "$configFile"; then
+    echo Invalid session.
+    echo Choose between:
+
+    list_sessions
+    return 1
+fi
+
+rm -rf demo
+mkdir demo
+
+cp $configFile ./demo/config.yml
+copy_runfile ./demo/run.sh
 
 (cd ./demo && . run.sh)
--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
+2019 Release Notes - master
 # Run
 `. demo.sh <learning session>`
 
 # Learning sessions (more to come)
-- rebaseconflict
+- merge-noff
+- rebase-merge-ff
+- rebase-squash-merge-ff
 - rebasenoconflict
