f935626b878d05eee562ea6eaa547f08fdfe6eb9
pluqueTheLuxe <pierre-luc.simard@videotron.ca>
2020-09-12 22:11:01 -0400

    release notes

--- demo.sh
+++ demo.sh
@@ -2,7 +2,10 @@ init-session() {
     git init &> /dev/null
     #wireing: do not take note
     git config alias.adog "log --all --decorate --oneline --graph"  &> /dev/null
-    echo "run.sh" > .gitignore
+    cat <<EOF > .gitignore
+run.sh
+config.yml
+EOF
     git add . &> /dev/null
     git commit -am "initial commit" &> /dev/null
 }
@@ -12,7 +15,7 @@ proceed() {
     echo
 }
 
-runfile=./sessions/$1.sh
+runfile=./sessions/$1.yml
 
 if ! test -f "$runfile"; then
     echo no test case
@@ -22,6 +25,26 @@ fi
 rm -rf demo
 mkdir demo
 
-cp $runfile ./demo/run.sh
+cp $runfile ./demo/config.yml
+
+cat <<"EOF" > ./demo/run.sh
+echo "$(yq r config.yml intro)"
+
+stepsCount=$(yq r config.yml --length steps)
+for (( step = 0; step <= $stepsCount - 1; step++ ))
+do  
+    proceed
+
+    eval "$(yq r config.yml steps[$step].commands)"
+
+    echo "$(yq r config.yml steps[$step].title) done!"
+    echo Current repo state is ...
+    git adog
+done
+
+echo
+echo Session done!
+echo
+EOF
 
 (cd ./demo && . run.sh)
