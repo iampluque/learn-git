20c8af62b1708537441fbcabd52b0ad62624b69f
pluqueTheLuxe <pierre-luc.simard@videotron.ca>
2020-09-12 21:36:28 -0400

    release notes

--- demo.sh
+++ demo.sh
@@ -1,4 +1,18 @@
-runfile=run-$1.sh
+init-session() {
+    git init &> /dev/null
+    #wireing: do not take note
+    git config alias.adog "log --all --decorate --oneline --graph"  &> /dev/null
+    echo "run.sh" > .gitignore
+    git add . &> /dev/null
+    git commit -am "initial commit" &> /dev/null
+}
+proceed() {
+    echo
+    read -p "Press any key to proceed the next step..."
+    echo
+}
+
+runfile=./sessions/$1.sh
 
 if ! test -f "$runfile"; then
     echo no test case
@@ -8,6 +22,6 @@ fi
 rm -rf demo
 mkdir demo
 
-cp run-$1.sh ./demo/run.sh
+cp $runfile ./demo/run.sh
 
 (cd ./demo && . run.sh)
